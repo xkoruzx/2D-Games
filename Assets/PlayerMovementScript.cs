97afbf45daaf650917299d694dc7b930c29ff4f2
xkoruzx <xkoruzxth@gmail.com>
2024-12-08 17:11:28 +0700

    Initial commit

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{   
+   [SerializeField] private float speed;
+   private Rigidbody2D body;
+
+   private void Awake()
+   {
+        body = GetComponent<Rigidbody2D>();
+   }
+
+   private void Update()
+   {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > -0.01f)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if (horizontalInput < 0.01f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+   }
+    
+}
