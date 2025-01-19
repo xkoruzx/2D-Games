d16d41b67d72ade1b1fc6b2c31acf88581e9adf5
xkoruzx <xkoruzxth@gmail.com>
2025-01-19 15:47:28 +0700

    12/1

---
+++ Assets/CameraController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+   [SerializeField] private float speed;
+
+   private float currentPosX;
+   private Vector3 velocity = Vector3.zero;
+
+   [SerializeField] private Transform player;
+   [SerializeField] private float aheadDistance;
+   [SerializeField] private float cameraSpeed;
+
+    private float lookAhead;
+    // Update is called once per frame
+    private void Update()
+    {
+        //Room Camera
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //follow player (For Nanon)
+        // transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        // lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+
+
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
--- Assets/NewLevelTrigger.cs
+++ Assets/NewLevelTrigger.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewLevelTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform previousRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+            {
+                cam.MoveToNewRoom(nextRoom);
+            }
+            else
+            {
+                cam.MoveToNewRoom(previousRoom);
+            }
+        }
     }
-}
+ }
